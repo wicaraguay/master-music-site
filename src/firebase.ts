720f20ac0d28f2a1d8b3e389d48cb6324cdace72
Willan Caraguay <wicaraguay@gmail.com>
2026-02-08 19:15:35 -0500

    fix: alert keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDf2KNTcr4JSzVxCY_VBIn9OVz76MN_llQ",
-    authDomain: "maestro-portfolio.firebaseapp.com",
-    projectId: "maestro-portfolio",
-    storageBucket: "maestro-portfolio.firebasestorage.app",
-    messagingSenderId: "421781444208",
-    appId: "1:421781444208:web:bd13c414d31dad201ec6b5"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
